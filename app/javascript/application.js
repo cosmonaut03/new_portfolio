d9f0e97db75003454c771b56a34960f99426d2b5
三田亮介 <mita@MacBookProSub.local>
2024-06-25 10:01:01 +0900

    Initial commit

---
+++ app/javascript/application.js
@@ -0,0 +1,7 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// Entry point for the build script in your package.json
+import 'bootstrap';
+import 'jquery';
+//import '@fortawesome/fontawesome-free/js/fontawesome';
+import "@hotwired/turbo-rails"
+import "./controllers"
