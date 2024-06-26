4fde5c6f27722bc5b5a0cd919f1a7f2bdac449ad
三田亮介 <mita@MacBookProSub.local>
2024-06-26 10:43:42 +0900

    トップページの作成

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -2,16 +2,16 @@ class UserSessionsController < ApplicationController
   def new; end
 
   def create
-      @user = login(params[:email], params[:password])
-      if @user
+    @user = login(params[:email], params[:password])
+    if @user
       redirect_back_or_to root_path
-      else
+    else
       render :new
-      end
+    end
   end
 
   def destroy
-      logout
-      redirect_to root_path
+    logout
+    redirect_to root_path
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_23_110814) do
+ActiveRecord::Schema[7.1].define(version: 20_240_623_110_814) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", null: false
-    t.string "crypted_password"
-    t.string "salt"
-    t.string "name"
-    t.string "user_name", null: false
-    t.string "avatar"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'email', null: false
+    t.string 'crypted_password'
+    t.string 'salt'
+    t.string 'name'
+    t.string 'user_name', null: false
+    t.string 'avatar'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['email'], name: 'index_users_on_email', unique: true
   end
-
 end
